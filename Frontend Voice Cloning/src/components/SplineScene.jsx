892ec2118099ebcdd93f147d291491d697db9202
Pragyan Chandra Dhar <142593392+pragyandhar@users.noreply.github.com>
2025-11-03 10:48:23 +0530

    Delete Frontend Voice Cloning directory

--- Frontend Voice Cloning/src/components/SplineScene.jsx
+++
@@ -1,9 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function SplineScene() {
-  return (
-    <div className="spline-container" style={{ width: '100%', height: '100vh' }}>
-      <Spline scene="https://prod.spline.design/29Tapg2-bsw8cBkJ/scene.splinecode" />
-    </div>
-  );
-}
